a698c9a80ebdf3f19feb8ad056b0de8ae50dd730
Ihor Breza <seniorigor@gmail.com>
2012-06-08 14:14:04 +0300

    fix init.rb

--- init.rb
+++ init.rb
@@ -18,30 +18,32 @@ class User
   include DataMapper::Resource
 
   property :id,         Serial
-  property :username,   String, required: true, unique: true, length: 2..20
-  property :email,      String, required: true, unique: true, format: :email_address
+  property :username,   String, required: true, length: 2..20, unique: true
   property :password,   String, required: true, length: 6..20
+  property :first_name, String, required: true, length: 2..20
+  property :last_name,  String, required: true, length: 2..20
   property :created_at, DateTime
 
   has n,   :tasks
 
-  def initialize(username, email = nil, password)
-    @username = username
-    @email    = email
-    @password = password
+  def initialize(username, password, first_name = nil, last_name = nil)
+    @username   = username
+    @password   = password
+    @first_name = first_name
+    @last_name  = last_name
   end
 
   def register
-    self.username = @username
-    self.password = @password
-    self.email    = @email
+    self.username   = @username
+    self.password   = @password
+    self.first_name = @first_name
+    self.last_name  = @last_name
     self.save
   end
 
   def login
-    user = User.first(username: username)
-    return false if user.nil?
-    password == user.password
+    user = User.first(username: @username)
+    user.nil? ? false : password == user.password
   end
 end
 
@@ -50,38 +52,34 @@ class Task
   include DataMapper::Resource
 
   property   :id,           Serial
-  property   :title,        String,  required: true
-  property   :body,         Text,    required: true
-  property   :priority,     Enum[1, 2, 3, 4, 5]
+  property   :content,      String,    required: true
+  property   :priority,     Enum[1, 2, 3]
   property   :created_at,   DateTime
   #property   :user_id,      Integer
   property   :recipient_id, Integer
-  property   :read,         Boolean, default: false
 
   belongs_to :user
 
-  def initialize(title, body, priority, user_id, recipient_id)
-    @title        = title
-    @body         = body
+  def initialize(content, priority, user_id, recipient_id)
+    @content      = content
     @priority     = priority
     @user_id      = user_id
     @recipient_id = recipient_id
   end
 
   def create
-    self.title        = @title
-    self.body         = @body
+    self.content      = @content
     self.priority     = @priority
     self.user_id      = @user_id
     self.recipient_id = @recipient_id
-    self.save
-  end
-
-  def mark_as_read
-    self.read = true
     self.save
   end
 end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+#валідація на всі поля (кирилиця...)
+#повертати помилку, яка виникає при реєстрації (обробка помилок)
+#валідація на content до 140 символів
+#обробка помилок і повернення значення
