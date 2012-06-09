c0f7a2e2bebe9d8c1da266291594f3912cee071e
Ihor Breza <seniorigor@gmail.com>
2012-06-09 13:54:10 +0300

    Combined model and controller in init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-require './controller.rb'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'bundler/setup'
@@ -12,10 +11,12 @@ require 'dm-timestamps'
 require 'dm-types'
 require 'dm-serializer/to_json'
 
+# Database
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite:///#{Dir.pwd}/index.db")
 DataMapper::Property::String.length(20)
 DataMapper::Property::Text.length(140)
 
+# Model
 class User
   include DataMapper::Resource
 
@@ -58,25 +59,95 @@ class Task
   property   :priority,     Enum[1, 2, 3]
   property   :created_at,   DateTime
   #property   :user_id,      Integer
-  property   :recipient_id, Integer
+  property   :receiver_id,  Integer
 
   belongs_to :user
 
-  def initialize(content, priority, user_id, recipient_id)
+  def initialize(content, priority, user_id, receiver_id)
     @content      = content
     @priority     = priority
     @user_id      = user_id
-    @recipient_id = recipient_id
+    @receiver_id  = receiver_id
   end
 
   def create
     self.content      = @content
     self.priority     = @priority
     self.user_id      = @user_id
-    self.recipient_id = @recipient_id
+    self.receiver_id  = @receiver_id
     self.save ? true : self.errors.each { |error| error }
   end
 end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+# Controller
+use Rack::Session::Pool, expire_after: 2592000
+
+# Filters
+before do
+  content_type :json
+end
+
+before '/protected/*' do
+  @auth = session[:curent_user].nil? ? false : true
+end
+
+# Helpers
+helpers do
+
+  def login_exists(login)
+    User.first(login: login).nil? ? {login_exists: false} : {login_exists: true}
+  end
+
+  def login(login, password)
+    @user = User.new(login, password)
+    if @user.login
+      @user = User.first(login: login)
+      session[:curent_user] = @user
+      {login: true}
+    else
+      {login: false}
+    end
+  end
+
+  def add_new_task(content, priority, user_id, receiver_id)
+    @user_id     = User.first(login: user_id)
+    @receiver_id = session[:curent_user]
+    if content.empty? || priority.empty?
+      {newtask: false}
+    else
+      @task = Task.new(content, priority, user_id, receiver_id)
+      @task.create
+      {newtask: true}
+    end
+  end
+end
+
+
+# Routes
+get '/login' do
+  login(params[:login], params[:password])
+end
+
+post '/protected/newtask' do
+	if @auth
+    add_new_task(params[:content],
+         	       params[:priority],
+         	       params[:whom],
+                 params[:byWhom],
+                 params[:title])
+  else
+    {auth: false}
+  end
+end
+
+get '/protected/logout' do
+  if @auth
+    session.clear
+    {logout: true}
+  else
+    {auth: false}
+  end
+end
