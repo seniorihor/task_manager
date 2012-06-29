94d9bb69b07e7ac29320520fe9c0766ea3072e51
Ihor Breza <seniorigor@gmail.com>
2012-06-29 16:53:38 +0300

    fix Task

--- init.rb
+++ init.rb
@@ -53,11 +53,11 @@ class Task
   include DataMapper::Resource
 
   property :id,             Serial
-  property :content,        Text,         required: true
+  property :content,        Text,            required: true
   property :priority,       Enum[0, 1, 2, 3]
   property :created_at,     DateTime
-  property :receiver_login, String,       required: true, length:  2..20, format: /[a-zA-Z]/
-  property :read,           Boolean,      required: true, default: false
+  property :receiver_login, String,          required: true, length:  2..20, format: /[a-zA-Z]/
+  property :read,           Boolean,         required: true, default: false
 
   belongs_to :user
 end
