363b579a1bfc28b29f22e9b947bd40d3ce92bee1
Ihor Breza <seniorigor@gmail.com>
2012-07-03 13:54:55 +0300

    add logout and register

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,6 +1,5 @@
 require_relative '../init.rb'
 require 'rack/test'
-require 'rspec'
 require 'json'
 
 set :environment, :test
@@ -11,18 +10,49 @@ RSpec.configure do |conf|
   DataMapper.auto_migrate!
 end
 
+
 describe User do
+
+  before do
+    @user = User.create(login: 'login', password: 'password', firstname: 'firstname', lastname: 'lastname', token: 'auth_token')
+  end
 
   def app
     Sinatra::Application
   end
 
-  it 'should get a failed response to login' do
+  def to_hash(json_data)
+    JSON.parse(json_data)
+  end
 
-    request  = { "taskmanager" => { "login" => "login", "password" => "password" }}
-    response = { "login" => { "error" => "Invalid login or password" }}
+  it 'login should be successful' do
+    request  = { taskmanager: { login:    @user.login,
+                                password: @user.password }}
+    response = { login: { error:      'Success',
+                          auth_token: @user.token,
+                          friends:    @user.friends }}
 
     post '/login', request.to_json
+    hash = to_hash(last_response.body)
+    hash['login']['auth_token'] = 'auth_token'
+    hash.to_json.should == response.to_json
+  end
+
+  it 'logout should be successful' do
+    puts "token: #{@user.token}"
+
+    request  = { taskmanager: { auth_token: @user.token }}
+    response = { logout: { error: 'Success' }}
+
+    post '/protected/logout', request.to_json
+    hash = to_hash(last_response.body)
+    hash.to_json.should == response.to_json
+  end
+
+  it 'register should be successful' do
+    request  = { taskmanager: { login: 'test', password: 'password', firstname: 'firstname', lastname: 'lastname' }}
+    response = { register: { error: 'Success' }}
+    post '/register', request.to_json
     last_response.body.should == response.to_json
   end
 end
