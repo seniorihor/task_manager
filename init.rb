297c16b6a5b7bab629da053673747cd5da246637
Ihor Breza <seniorigor@gmail.com>
2012-07-31 23:31:48 +0300

    fix database connection

--- init.rb
+++ init.rb
@@ -20,12 +20,12 @@ configure :production do
 end
 
 configure :development do
-  DataMapper.setup(:default, "sqlite://#{Dir.pwd}/development.db")
+  DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/development.db")
   DataMapper::Logger.new($stdout, :debug)
 end
 
 configure :test do
-  #DataMapper.setup(:default, "sqlite://#{Dir.pwd}/test.db")
+  #DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/test.db")
   DataMapper.setup(:default, 'sqlite::memory:')
 end
 
