5de18f7c333ba589dd6178bfeea2858823c3838e
Taras B. Y. <taras.my.mail@gmail.com>
2012-08-04 18:11:39 +0300

    fix_style

--- curl_script.rb
+++ curl_script.rb
@@ -2,25 +2,25 @@ require 'json'
 require 'curb'
 
 def url_path(url)
-	if url == 'login' || 'register'
-		adress = 'http://task-manager-modular.herokuapp.com/'+url
-	else
-		adress = 'http://task-manager-modular.herokuapp.com/protected/'+url	
-	end	
+  if url == 'login' || 'register'
+    adress = 'http://task-manager-modular.herokuapp.com/'+url
+  else
+    adress = 'http://task-manager-modular.herokuapp.com/protected/'+url	
+  end	
 end
 
 def content(args)
-	array = args.split(' ')
-	hash = Hash.new
+  array = args.split(' ')
+  hash = Hash.new
 	
-	array.each do |el|
-		if array.index(el)%2 == 0
-			hash[el] = array[array.index(el)+1]
-		end		
-	end
-	jdata = Hash.new
-	jdata['taskmanager'] = hash
-	jdata.to_json
+  array.each do |el|
+    if array.index(el)%2 == 0
+	  hash[el] = array[array.index(el)+1]
+	end		
+  end
+  jdata = Hash.new
+  jdata['taskmanager'] = hash
+  jdata.to_json
 end
 
 puts "ADRESS:  "
@@ -28,11 +28,10 @@ url = gets.chomp
 puts "Arguments: "
 args = gets.chomp
 
-response = Curl::Easy.http_post(url_path(url), content(args)
-    ) do |curl|
+response = Curl::Easy.http_post(url_path(url), content(args)) do |curl|
       curl.headers['Accept'] = 'application/json'
       curl.headers['Content-Type'] = 'application/json'
 end
 
-puts url+' Response : '+response.body_str
-puts 'Response code : '+response.response_code.to_s
+puts url+" Response : #{response.body_str}"
+puts "Response code : #{response.response_code}"
