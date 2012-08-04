1f20710f1ba91fb585e2b2e930c1adacac226530
Taras B. Y. <taras.my.mail@gmail.com>
2012-08-04 16:39:39 +0300

    fix_script_structure

--- curl_script.rb
+++ curl_script.rb
@@ -1,33 +1,37 @@
 require 'json'
 require 'curb'
 
+def url_path(url)
+	if url == 'login' || 'register'
+		adress = 'http://task-manager-modular.herokuapp.com/'+url
+	else
+		adress = 'http://task-manager-modular.herokuapp.com/protected/'+url	
+	end	
+end
+
+def str_to_json(args)
+	array = args.split(' ')
+	hash = Hash.new
+	
+	array.each do |el|
+		if array.index(el)%2 == 0
+			hash['taskmanager'][el] = array[array.index(el)+1]
+		end		
+	end 
+	return hash.to_json
+end
+
 puts "ADRESS:  "
 url = gets.chomp
-
 puts "Arguments: "
 args = gets.chomp
-array = args.split(' ')
-hash = Hash.new
 
-array.each do |el|
-	if array.index(el)%2 == 0
-		hash[el] = array[array.index(el)+1]
-	end		
-end 
-
-jdata = Hash.new
-jdata['taskmanager'] = hash
-
-if url == 'login' || 'register'
-	adress = 'http://task-manager-modular.herokuapp.com/'+url
-else
-	adress = 'http://task-manager-modular.herokuapp.com/protected/'+url	
-end	
-	
-response = Curl::Easy.http_post(adress, jdata.to_json
+response = Curl::Easy.http_post(url_path(url), str_to_json(args)
     ) do |curl|
       curl.headers['Accept'] = 'application/json'
       curl.headers['Content-Type'] = 'application/json'
 end
+
 puts url+' Response : '+response.body_str
 puts 'Response code : '+response.response_code.to_s
+
